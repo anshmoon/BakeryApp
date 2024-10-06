e82d2a233cbde074f9edad3c3733418dcdcd1321
Nitesh Choubey <89741235+anshmoon@users.noreply.github.com>
2024-10-06 21:10:36 +0530

    added email order support

--- react-recipe-app-main/src/components/RecipeCard.js
+++ react-recipe-app-main/src/components/RecipeCard.js
@@ -4,11 +4,19 @@ import Popup from "./Popup";
 
 
 export default function RecipeCard({recipe}){
-    const [recipeOpen,setRecipeOpen]= useState(false);
     let text="Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
-    const togglePopup = () => {
-        setRecipeOpen(!recipeOpen);
-      };
+    const sendEmail = (dishName) => {
+        const email = 'rekhachoubey42.com'; 
+        const subject = 'Baking Order'; 
+        const body = `i want to order ${dishName}`;
+    
+        
+        const mailto = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+        
+        
+        window.location.href = mailto;
+      }
+    
     return (
         <div className="recipe-card">
             <CustomImage imgSrc={recipe.image} pt="65%"/>
@@ -16,8 +24,7 @@ export default function RecipeCard({recipe}){
                 <img className="auther-img" src={recipe.authorImg} alt=""/>
                 <p className="recipe-title">{recipe.title}</p>
                 <p className="recipe-desc">{text}</p>
-                <a className="view-btn" href="#!" onClick={()=>togglePopup()}>VIEW RECIPE</a>
-                {/* {recipeOpen && <Popup recipe={text} recipeOpen={recipeOpen} />} */}
+                <a className="view-btn" href="#" onClick={()=>sendEmail(recipe.title)}>Order Now</a>
             </div>
         </div>
     )
